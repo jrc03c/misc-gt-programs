de2f4d8226ed0a9d1533905b5bbb7cca31d88386
jrc03c <jrc03c@pm.me>
2024-05-17 17:15:59 -0500

    ...

--- other/strong-interest-inventory/build.js
+++ other/strong-interest-inventory/build.js
@@ -1,12 +1,61 @@
-const process = require("process")
+const { set, sort } = require("@jrc03c/js-math-tools")
+const { snakeify } = require("@jrc03c/js-text-tools")
+const fs = require("node:fs")
+const gt = require("@jrc03c/gt-helpers")
+const path = require("node:path")
+const process = require("node:process")
 const watch = require("@jrc03c/watch")
 
-function rebuild() {
+async function rebuild() {
   console.log("-----")
   console.log(`Rebuilding... (${new Date().toLocaleString()})`)
 
   try {
-    // do stuff...
+    const template = fs.readFileSync(
+      path.join(__dirname, "template.gt"),
+      "utf8",
+    )
+
+    const strong = JSON.parse(
+      fs.readFileSync(path.join(__dirname, "strong.json"), "utf8"),
+    )
+
+    const categories = set(
+      sort(Object.keys(strong).map(key => snakeify(key.split(":")[0]))),
+    )
+
+    const questions = sort(
+      set(
+        Object.keys(strong).map(key => {
+          return strong[key].map(text => {
+            return {
+              text,
+              category: snakeify(key.split(":")[0]),
+            }
+          })
+        }),
+      ),
+      (a, b) => (a.text < b.text ? -1 : 1),
+    )
+
+    const categories_scores_dict = `{ ${categories.map(c => `"strong_${c}_score" -> strong_${c}_score`).join(", ")} }`
+
+    const data = { categories, questions, categories_scores_dict }
+    let rendered = await gt.template.liquidBuild(template, data)
+
+    rendered = rendered
+      .split("\n")
+      .filter(
+        (line, i, arr) =>
+          i === 0 || !arr[i - 1].trim().match(/^>>\s*strong.*?=\s*0/g),
+      )
+      .join("\n")
+
+    while (rendered.match(/\n\n\n/g)) {
+      rendered = rendered.replaceAll(/\n\n\n/g, "\n\n")
+    }
+
+    fs.writeFileSync(path.join(__dirname, "program.gt"), rendered, "utf8")
     console.log("Done! 🎉")
   } catch (e) {
     console.error(e)
@@ -15,8 +64,8 @@ function rebuild() {
 
 if (process.argv.indexOf("-w") > -1 || process.argv.indexOf("--watch") > -1) {
   watch({
-    target: ".",
-    exclude: ["node_modules"],
+    target: __dirname,
+    exclude: ["program.gt"],
     created: rebuild,
     modified: rebuild,
     deleted: rebuild,
