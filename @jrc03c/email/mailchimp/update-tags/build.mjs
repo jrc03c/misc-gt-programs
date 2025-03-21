c13aaab6b73da0f6ae47e5c54d599eca925e2739
jrc03c <jrc03c@pm.me>
2025-03-21 09:29:01 -0500

    ...

--- @jrc03c/email/mailchimp/update-tags/build.mjs
+++ @jrc03c/email/mailchimp/update-tags/build.mjs
@@ -1,4 +1,9 @@
+import { createDocsTable } from "../../../../_build_helpers/create-docs-table.mjs"
+import { DataFrame } from "@jrc03c/js-math-tools"
+import { removeLeadingAndTrailingSpaces } from "../../../../_build_helpers/remove-leading-and-trailing-spaces.mjs"
 import { render } from "../../../../_build_helpers/render.mjs"
+import { typePhrase } from "../../../../_build_helpers/type-phrase.mjs"
+import { unindent } from "@jrc03c/js-text-tools"
 import { watch } from "@jrc03c/watch"
 import fs from "node:fs"
 import path from "node:path"
@@ -17,7 +22,179 @@ function rebuild() {
       "utf8",
     )
 
-    const programData = {}
+    const inputVariables = [
+      {
+        name: "mailchimp_service_name",
+        type: "string",
+        description:
+          "the name assigned to the Mailchimp service in the program's 'Service' settings",
+        required: false,
+        default: "Mailchimp",
+        shouldBeCleanedUp: true,
+      },
+      {
+        name: "email_address",
+        type: "string",
+        description: "the email address for which the tags will be updated",
+        required: true,
+        shouldBeCleanedUp: false,
+      },
+      {
+        name: "mailchimp_list_id",
+        type: "string",
+        description:
+          "the ID of the list to which the email address is subscribed",
+        required: true,
+        shouldBeCleanedUp: true,
+      },
+      {
+        name: "tags",
+        type: "collection",
+        description: `the tags to be updated; can include strings or association values (where each assocation has (1) a "name" key pointing to a string value representing the name of the tag to be updated, and (2) a "status" key pointing to a string value of "active" or "inactive")`,
+        required: true,
+        shouldBeCleanedUp: true,
+        customCheck:
+          "---\n\n" +
+          unindent(
+            removeLeadingAndTrailingSpaces(`
+						*if: not tags
+							>> error_message = "You must define a variable called \`tags\` that has a collection value representing the tags to be updated! See the program documentation for more information about what can be included in the \`tags\` collection."
+							*program: @jrc03c/show-error
+							*return
+
+						*if: not (tags.type = "collection")
+							>> error_message = "You must define a variable called \`tags\` that has a collection value representing the tags to be updated! See the program documentation for more information about what can be included in the \`tags\` collection. (The value you provided was a(n) {tags.type}.)"
+							*program: @jrc03c/show-error
+							*return
+
+						*for: tag in tags
+							*if: (not tag.type = "string") and (not tag.type = "assocation")
+								>> error_message = ""
+								*program: @jrc03c/show-error
+								*return
+
+							*if: tag.type = "association"
+								*if: not tag["name"]
+									>> error_message = "Each assocation included in the \`tags\` collection must have a 'name' key pointing to a string value representing the name of the tag to be updated!"
+									*program: @jrc03c/show-error
+									*return
+
+								*if: not (tag["name"].type = "string")
+									>> error_message = "Each assocation included in the \`tags\` collection must have a 'name' key pointing to a string value representing the name of the tag to be updated! (The value you provided was a(n) {tag["name"].type}.)"
+									*program: @jrc03c/show-error
+									*return
+
+								*if: not tag["status"]
+									>> error_message = "Each assocation included in the \`tags\` collection must have a 'status' key pointing to a string value of either 'active' or 'inactive'!"
+									*program: @jrc03c/show-error
+									*return
+
+								*if: not (tag["status"].type = "string")
+									>> error_message = "Each assocation included in the \`tags\` collection must have a 'status' key pointing to a string value of either 'active' or 'inactive'! (The value you provided as a(n) {tag["status"].type}.)"
+									*program: @jrc03c/show-error
+									*return
+					`),
+          ),
+      },
+    ].toSorted((a, b) => (a.name < b.name ? -1 : 1))
+
+    const outputVariables = [
+      {
+        name: "was_updated",
+        type: "string",
+        description: `a string value of "yes" or "no" indicating whether or not the user's tags were successfully updated`,
+      },
+      {
+        name: "mailchimp_tags_update_error",
+        type: "association",
+        description: `an association with response data from Mailchimp; will be an empty association if the tag update request succeeds`,
+      },
+    ].toSorted((a, b) => (a.name < b.name ? -1 : 1))
+
+    const inputVariableChecks = inputVariables
+      .map(v => {
+        if (v.customCheck) {
+          return v.customCheck
+        }
+
+        const undefinedRequiredBlock = unindent(
+          removeLeadingAndTrailingSpaces(`
+					*if: not ${v.name}
+						>> error_message = "You must define a variable called \`${v.name}\` that has ${typePhrase(v.type)} value representing ${v.description}!"
+						*program: @jrc03c/show-error
+						*return
+				`),
+        )
+
+        const undefinedOptionalBlock = unindent(
+          removeLeadingAndTrailingSpaces(`
+					*if: not ${v.name}
+						>> ${v.name} = ${v.type === "string" ? JSON.stringify(v.default) : v.default}
+				`),
+        )
+
+        const dataTypeBlock = unindent(
+          removeLeadingAndTrailingSpaces(`
+					*if: not (${v.name}.type = "${v.type}")
+						>> error_message = "The variable \`${v.name}\` must have ${typePhrase(v.type)} value representing ${v.description}. (The value you provided was a(n) {${v.name}.type}.)"
+						*program: @jrc03c/show-error
+						*return
+				`),
+        )
+
+        return unindent(
+          removeLeadingAndTrailingSpaces(
+            "---\n\n" +
+              (v.required ? undefinedRequiredBlock : undefinedOptionalBlock) +
+              "\n\n" +
+              dataTypeBlock,
+          ),
+        )
+      })
+      .join("\n\n")
+
+    const nameColumnWidth = Math.max(
+      ...inputVariables.concat(outputVariables).map(v => v.name.length),
+    )
+
+    const docsInputsTable = createDocsTable(
+      new DataFrame({
+        name: inputVariables.map(v => v.name + (v.required ? "*" : "")),
+        description: inputVariables.map(v => v.description),
+      }),
+      {
+        nameColumnLabel: "INPUT 👉",
+        nameColumnWidth,
+        descriptionColumnLabel: "DESCRIPTION ℹ️",
+      },
+    ).replace("-- ", "")
+
+    const docsOutputsTable = createDocsTable(
+      new DataFrame({
+        name: outputVariables.map(v => v.name),
+        description: outputVariables.map(v => v.description),
+      }),
+      {
+        nameColumnLabel: "OUTPUT 👈",
+        nameColumnWidth,
+        descriptionColumnLabel: "DESCRIPTION ℹ️",
+      },
+    ).replace("-- ", "")
+
+    const cleanup = inputVariables
+      .filter(
+        v => typeof v.shouldBeCleanedUp === "undefined" || v.shouldBeCleanedUp,
+      )
+      .map(v => `>> ${v.name} = ""`)
+      .join("\n")
+
+    const programData = {
+      docsInputsTable,
+      docsOutputsTable,
+      inputVariableChecks,
+      cleanup,
+    }
+
     const program = render(programTemplate, programData)
     fs.writeFileSync(path.join(dir, "program.gt"), program, "utf8")
 
--- _build_helpers/render.mjs
+++ _build_helpers/render.mjs
@@ -2,9 +2,18 @@ function render(template, data) {
   let out = template.slice()
 
   Object.keys(data).forEach(key => {
-    const pattern = new RegExp(`\\{\\{\\s*?${key}\\s*?\\}\\}`)
-    out = out.replaceAll(pattern, data[key])
+    out = out.replaceAll(`{{ ${key} }}`, data[key])
   })
+
+  const unrenderedVariables = out.match(
+    /\{\{\s*[a-zA-Z_]+[a-zA-Z0-9_]*\s*\}\}/g,
+  )
+
+  if (unrenderedVariables && unrenderedVariables.length > 0) {
+    throw new Error(
+      `These variables were present in the \`template\` but not in the \`data\` object passed into the \`render\` function:\n\n${unrenderedVariables.map(v => "→ " + v).join("\n")}\n`,
+    )
+  }
 
   return out
 }
