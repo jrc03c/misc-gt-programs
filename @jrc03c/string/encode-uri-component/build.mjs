505aafb59cfa74cc4e610c34f2a798febdcecc70
jrc03c <jrc03c@pm.me>
2025-03-25 11:35:31 -0500

    ...

--- @jrc03c/string/encode-uri-component/build.mjs
+++ @jrc03c/string/encode-uri-component/build.mjs
@@ -1,3 +1,7 @@
+import { createDocsTable } from "../../../_build_helpers/create-docs-table.mjs"
+import { DataFrame } from "@jrc03c/js-math-tools"
+import { makeKey } from "@jrc03c/make-key"
+import { range } from "@jrc03c/js-math-tools"
 import { render } from "../../../_build_helpers/render.mjs"
 import fs from "node:fs"
 import path from "node:path"
@@ -32,12 +36,64 @@ import path from "node:path"
     .concat([`>> dict["%22".decode("URL")] = "%22"`])
     .join("\n")
 
+  const nameColumnWidth = "OUTPUT 👈".length
+
+  const docsInputsTable = createDocsTable(
+    new DataFrame({
+      name: ["input*"],
+      description: ["the string to be encoded"],
+    }),
+    {
+      nameColumnLabel: "INPUT 👉",
+      nameColumnWidth,
+      descriptionColumnLabel: "DESCRIPTION ℹ️",
+    },
+  )
+
+  const docsOutputsTable = createDocsTable(
+    new DataFrame({
+      name: ["output"],
+      description: ["the encoded string"],
+    }),
+    {
+      nameColumnLabel: "OUTPUT 👈",
+      nameColumnWidth,
+      descriptionColumnLabel: "DESCRIPTION ℹ️",
+    },
+  )
+
   const programData = {
     dictAssignments,
-    docsInputsTable: "",
-    docsOutputsTable: "",
+    docsInputsTable,
+    docsOutputsTable,
   }
 
   const program = render(programTemplate, programData)
   fs.writeFileSync(path.join(dir, "program.gt"), program, "utf8")
+
+  const testsTemplate = fs.readFileSync(
+    path.join(dir, "tests-template.gt"),
+    "utf8",
+  )
+
+  const usedGlyphs = Object.keys(encodings).join("")
+
+  const pairsAssignments = range(0, 100)
+    .map(() => {
+      const value = makeKey(
+        Math.floor(Math.random() * 224) + 32,
+        null,
+        usedGlyphs,
+      )
+
+      return `>> pairs.add(["${value}", "${encodeURIComponent(value)}"])`
+    })
+    .join("\n")
+
+  const testsData = {
+    pairsAssignments,
+  }
+
+  const tests = render(testsTemplate, testsData)
+  fs.writeFileSync(path.join(dir, "tests.gt"), tests, "utf8")
 })()
